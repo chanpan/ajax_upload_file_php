989ad09699db4ff4bb5b33f3cccb61646672f88a
chanpan <chanpan.nuttaphon@gmail.com>
2017-11-09 22:29:09 +0700

    upload

--- server/upload.php
+++ server/upload.php
@@ -1,7 +1,25 @@
 <?php 
 header('Access-Control-Allow-Origin: *'); 
+$out = []; 
+foreach($_FILES as $file){
+	$imageFileType = pathinfo($_FILES[0]['name'],PATHINFO_EXTENSION); 
+	$fileName = "Demo_".random_string(50).".$imageFileType";
+	move_uploaded_file($file["tmp_name"], "uploads/$fileName"); 
+	array_push($out, $fileName);
+}
 
-//ถ้ามีหลายไฟล์ก็ foreach() เอา
-move_uploaded_file($_FILES[0]["tmp_name"], "uploads/demo.jpg"); 
+echo json_encode($out);
+ 
+
+function random_string($length) {
+    $key = '';
+    $keys = array_merge(range(0, 9), range('a', 'z'));
+
+    for ($i = 0; $i < $length; $i++) {
+        $key .= $keys[array_rand($keys)];
+    }
+
+    return $key;
+}
 
 ?>
